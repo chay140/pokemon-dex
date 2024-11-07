44a724ba9b96e1f48ef2223658d333c7a11e1f9b
chay140 <chaihyunpark140@gmail.com>
2024-11-07 22:58:42 +0900

    add: completed default dashboard with 6 cells

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Pokeball from "../assets/pokeball.png"
 
 function Dashboard() {
 	const StyledDashboard = styled.div`
@@ -12,9 +13,54 @@ function Dashboard() {
     border-radius: 10px;
   `;
 
+  const StCellWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 10px;
+    width: 100%;
+    justify-items: center;
+  `;
+
+  const StImgWrapper = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: rgb(255, 255, 255);
+    border: 6px double rgb(204, 204, 204);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+  `;
+  
+  const StPokeballImg = styled.img`
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+  `;
+
   return (
     <StyledDashboard>
       <h3>나만의 포케몬</h3>
+      <StCellWrapper>
+        <StImgWrapper>
+          <StPokeballImg src={Pokeball} alt="Pokeball 8bit icon"/>
+        </StImgWrapper>
+        <StImgWrapper>
+          <StPokeballImg src={Pokeball} alt="Pokeball 8bit icon"/>
+        </StImgWrapper>
+        <StImgWrapper>
+          <StPokeballImg src={Pokeball} alt="Pokeball 8bit icon"/>
+        </StImgWrapper>
+        <StImgWrapper>
+          <StPokeballImg src={Pokeball} alt="Pokeball 8bit icon"/>
+        </StImgWrapper>
+        <StImgWrapper>
+          <StPokeballImg src={Pokeball} alt="Pokeball 8bit icon"/>
+        </StImgWrapper>
+        <StImgWrapper>
+          <StPokeballImg src={Pokeball} alt="Pokeball 8bit icon"/>
+        </StImgWrapper>
+      </StCellWrapper>
     </StyledDashboard>
   );
 }
