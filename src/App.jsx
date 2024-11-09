448175de7dde75f52a9c9edd0ee4711cca2068ae
chay140 <chaihyunpark140@gmail.com>
2024-11-09 22:39:57 +0900

    feat: user data stored using Context API & added add button in detail pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 import Router from "./shared/Router";
+import { PokemonContext } from "./context/PokemonContext";
 
 function App() {
-  return <Router />;
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const addPokemonHandler = (pokemon) => {
+    setPokemonList((prevList) => {
+      if (prevList.length < 6 && !prevList.includes(pokemon)) {
+        return [...prevList, pokemon];
+      } else {
+        if (prevList.includes(pokemon)) {
+          // 중복 확인
+          alert("중복된 포켓몬이 있습니다.");
+        } else {
+          // 6개 초과시
+          alert("6개 초과");
+        }
+
+        return [...prevList];
+      }
+    });
+  };
+
+  const removePokemonHendler = (pokemon) => {
+    setPokemonList((prevList) => {
+      const newList = prevList.filter((item) => {
+        return JSON.stringify(item) !== JSON.stringify(pokemon);
+      });
+
+      return [...newList];
+    });
+  };
+
+  return (
+    <PokemonContext.Provider
+      value={{ pokemonList, addPokemonHandler, removePokemonHendler }}
+    >
+      <Router />
+    </PokemonContext.Provider>
+  );
 }
 
 export default App;
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,44 +1,12 @@
-import { useState } from "react";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
-import { PokemonContext } from "../context/PokemonContext";
 
 function Dex() {
-  const [pokemonList, setPokemonList] = useState([]);
-
-  const addPokemonHandler = (pokemon) => {
-    setPokemonList((prevList) => {
-      if (prevList.length < 6 && !prevList.includes(pokemon)) {
-        return [...prevList, pokemon];
-      } else {
-        if (prevList.includes(pokemon)) {
-          // 중복 확인
-          alert("중복된 포켓몬이 있습니다.");
-        } else {
-          // 6개 초과시
-          alert("6개 초과");
-        }
-
-        return [...prevList];
-      }
-    });
-  };
-
-  const removePokemonHendler = (pokemon) => { 
-    setPokemonList((prevList) => {
-      const newList = prevList.filter((item) => {
-        return JSON.stringify(item) !== JSON.stringify(pokemon);
-      });
-
-      return [...newList];
-    });
-  };
-
   return (
-    <PokemonContext.Provider value={{pokemonList, addPokemonHandler, removePokemonHendler}}>
+    <>
       <Dashboard />
-      <PokemonList addPokemonHandler={addPokemonHandler} />
-    </PokemonContext.Provider>
+      <PokemonList />
+    </>
   );
 }
 
